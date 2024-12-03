b5f8497ebb693f8611c9ffa308c0901f0b990450
Jakub Kogut <jakub@jkogut.pl>
2024-12-03 13:10:37 +0100

    maven struktura

---
+++ src/main/java/com/jkpr/chinesecheckers/App.java
@@ -0,0 +1,13 @@
+package com.example.chinesecheckers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
